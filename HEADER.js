2dced545778bbca272fe64d847f468cf5dbc95be
moontai0724 <moontai0724@gmail.com>
2023-04-15 20:00:01 +0800

    chore: setup project

---
+++ HEADER.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Hololive Shop Price Differ
+// @namespace    https://github.com/moontai0724
+// @version      1.0.0
+// @description  A script to quickly diff the price in different currency in Hololive shop.
+// @author       moontai0724
+// @match        https://shop.hololivepro.com/products/*
+// @grant        none
+// @supportURL   https://github.com/moontai0724/hololive-shop-price-diff-script/issues
+// @license      MIT
+// ==/UserScript==
